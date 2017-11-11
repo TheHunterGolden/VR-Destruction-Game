03ef303cf789615c0345d02edce88cc8d9cf0868
TheHunterGolden <huntergolden0124@gmail.com>
2017-11-11 14:38:29 +0000

    added destructible wall

---
+++ Assets/Scripts/BreakableObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakableObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider col) {
+        if (col.gameObject.tag == "hammer") {
+            if (!gameObject.GetComponent<Rigidbody>())
+            {
+                gameObject.AddComponent<Rigidbody>();
+                gameObject.GetComponent<Rigidbody>().collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
+               
+            }
+            
+        }
+        
+    }
+}
