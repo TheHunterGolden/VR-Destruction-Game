1bac098d01920b6194a1b07ac30a0b7b78d95898
TheHunterGolden <huntergolden0124@gmail.com>
2017-11-15 17:03:43 +0000

    upgraded walls/implemented framerate fix

--- Assets/Scripts/BreakableObject.cs
+++ Assets/Scripts/BreakableObject.cs
@@ -2,28 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class BreakableObject : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
-    void OnTriggerEnter(Collider col) {
-        if (col.gameObject.tag == "hammer") {
-            if (!gameObject.GetComponent<Rigidbody>())
-            {
-               // gameObject.AddComponent<Rigidbody>();
-               // gameObject.GetComponent<Rigidbody>().collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
-               
-            }
-            
-        }
+
+    public GameObject areaOfEffect;
+
+
+    void OnCollisionEnter(Collision col) {
+
+        ContactPoint contact = col.contacts[0];
         
+        if (col.gameObject.tag == "hammer")
+        {
+            if ((col.relativeVelocity.magnitude > 1) && (gameObject.GetComponent<Rigidbody>().isKinematic == true))
+            {
+     
+                gameObject.GetComponent<Rigidbody>().isKinematic = false;
+                GameObject aoe = Instantiate(areaOfEffect, contact.point, Quaternion.Euler(gameObject.transform.rotation.eulerAngles.x, gameObject.transform.rotation.eulerAngles.y, gameObject.transform.rotation.eulerAngles.z + 90));
+                aoe.GetComponent<AreaOfEffect>().setSize(col.relativeVelocity.magnitude);
+                Destroy(aoe);
+            }
+        }
     }
 }
