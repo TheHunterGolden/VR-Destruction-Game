24a3723027a72baca94c30b18274c1d0367ff5fa
Yixiang Xu <yixiangxu@umass.edu>
2018-02-02 19:25:45 -0500

    added particle effect when hitting the wall

--- Assets/Scripts/BreakableObject.cs
+++ Assets/Scripts/BreakableObject.cs
@@ -15,8 +15,12 @@ public class BreakableObject : MonoBehaviour {
     public bool useTimer;
 	private VRTK_InteractableObject objScript;
 	private VRTK_FixedJointGrabAttach fixedGrabAttach;
-    
-	void Start() {
+
+    [SerializeField]
+    [Tooltip("The prefab for particle effect when hitting the wall")]
+    GameObject prefabHitEffect;
+
+    void Start() {
         timer = 3;
         startTimer = false;
 		if (gameObject.GetComponent<VRTK_InteractableObject> () && gameObject.GetComponent<VRTK_FixedJointGrabAttach> ()) { 
@@ -56,6 +60,7 @@ public class BreakableObject : MonoBehaviour {
                 gameObject.GetComponent<Rigidbody>().isKinematic = false;
 				GameObject aoe = Instantiate(areaOfEffect, contact.point , Quaternion.Euler(gameObject.transform.rotation.eulerAngles.x, gameObject.transform.rotation.eulerAngles.y, gameObject.transform.rotation.eulerAngles.z + 90));
                 aoe.GetComponent<AreaOfEffect>().setSize(col.relativeVelocity.magnitude);
+                Instantiate(prefabHitEffect, contact.point, Quaternion.Euler(gameObject.transform.rotation.eulerAngles.x, gameObject.transform.rotation.eulerAngles.y, gameObject.transform.rotation.eulerAngles.z + 90));
 				//Debug.Log (col.relativeVelocity.magnitude);
                 Destroy(aoe);
             }
