7084ac8da07c7804336254d70d64d78c0c5d85a1
TheHunterGolden <huntergolden0124@gmail.com>
2018-02-19 15:59:58 -0500

    audio working

---
+++ Assets/Scripts/BreakSound.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakSound : MonoBehaviour {
+	public AudioClip[] audioClips;
+	public AudioSource audioSource;
+
+	void OnCollisionEnter(Collision col) {
+		if(col.gameObject.tag == "hammer")
+			audioSource.PlayOneShot (audioClips [Random.Range (0, audioClips.Length)]);
+		
+	}
+}
