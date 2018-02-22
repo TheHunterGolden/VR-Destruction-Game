f6a01fad75431a09983bece5617f3dde17c62e5c
TheHunterGolden <huntergolden0124@gmail.com>
2018-02-22 17:03:31 -0500

    mining level, minor fixes

---
+++ Assets/Scripts/MineCart.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MineCart : MonoBehaviour {
+
+	private Rigidbody rigidbody;
+	public GameObject textHolder;
+
+	void Start () {
+		rigidbody = gameObject.GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter (Collider collider) {
+
+		if (collider.gameObject.tag == "breakable") {
+			rigidbody.mass += collider.gameObject.GetComponent<Rigidbody> ().mass;
+			GameObject text = Instantiate (textHolder, collider.gameObject.transform.localPosition, Quaternion.identity);
+			Destroy (collider.gameObject);
+		}
+	}
+}
