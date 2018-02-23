08b2dea5013f62997ca40ed4f70d4b6ff644ea94
TheHunterGolden <huntergolden0124@gmail.com>
2018-02-23 15:46:21 -0500

    controller UI and points

---
+++ Assets/Elevator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour {
+	public float speed;
+	public float bottom;
+	public float top;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
