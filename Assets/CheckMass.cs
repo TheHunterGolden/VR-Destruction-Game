4c845f054f625f18d2f04dabc3c6c52fe3f775fd
TheHunterGolden <huntergolden0124@gmail.com>
2018-03-05 01:37:56 -0500

    hammer teleporters

---
+++ Assets/CheckMass.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckMass : MonoBehaviour {
+
+
+    void OnCollisionEnter(Collision col) {
+        if (col.gameObject.GetComponent<Rigidbody>()) {
+            if (col.gameObject.GetComponent<Rigidbody>().mass >= 400) {
+                gameObject.GetComponent<Rigidbody>().isKinematic = false;
+            }
+        }
+    }
+}
---
+++ Assets/HammerTeleporter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HammerTeleporter : MonoBehaviour {
+
+    public float timer;
+    public Transform playerPos;
+    public Transform teleportDestination;
+
+    void Start() {
+        timer = 3.0f;
+    }
+
+    void Update() {
+        if (timer >= 0)
+        {
+            timer -= Time.deltaTime;
+        }
+    }
+
+    void OnTriggerEnter(Collider col) {
+        if ((col.gameObject.tag == "hammer") && (timer <= 0.2f)) {
+            //Debug.Log("hit hammer");
+            playerPos.localPosition = teleportDestination.localPosition;
+            teleportDestination.GetComponent<HammerTeleporter>().timer = 3.0f;
+        }
+    }
+
+
+}
